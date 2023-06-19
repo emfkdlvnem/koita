ae73198f45d956ece98a75c32d64fd528d78d0fb
YoonHyeJin <emfkdlvnem@gmail.com>
2023-06-19 11:36:26 +0900

    캐러셀 완료

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
 // main page index
 
 // carousel
-let slideIndex = 0; // 현재 보여지는 슬라이드 초기값 0으로
-const slides = document.querySelectorAll('.slide');
-const indicators = document.querySelectorAll('.indicator');
-const prevBtn = document.querySelector('.btn.prev');
-const nextBtn = document.querySelector('.btn.next');
+document.addEventListener('DOMContentLoaded', () => {
+    let slideIndex = 0; 
+    const slides = document.querySelectorAll('.slide');
+    const indicators = document.querySelectorAll('.indicator');
+    const prevBtn = document.querySelector('.btn.prev');
+    const nextBtn = document.querySelector('.btn.next');
 
-// 슬라이드 자동 넘김
-let autoSlideTimer;
+    // 슬라이드 자동 넘김
+    let autoSlideTimer;
 
-function goToSlide(index) {
-    // 슬라이드 숨김
-    slides.forEach(slide => {
-        slide.style.display = 'none';
+    function goToSlide(index) {
+        // 슬라이드 숨김
+        slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+
+        // 해당 슬라이드만 보이게
+        slides[index].style.display = 'block'; 
+
+        // 인디케이터 active 비활성화
+        indicators.forEach(indicator => {
+            indicator.classList.remove('active');
+        });
+
+        // active될 인디케이터 표시
+        indicators[index].classList.add('active');
+
+        slideIndex = index;
+
+        // 페이지 넘겼을 때 타이머 재설정
+        resetAutoSlideTimer();
+    }
+
+    // 이전 버튼 클릭 시
+    prevBtn.addEventListener('click', () => {
+        const prevIndex = (slideIndex - 1 + slides.length) % slides.length;
+        goToSlide(prevIndex);
     });
 
-    // 해당 슬라이드만 보이게
-    slides[index].style.display = 'block'; 
-
-    // 인디케이터 active 비활성화
-    indicators.forEach(indicator => {
-        indicator.classList.remove('active');
+    // 다음 버튼 클릭 시
+    nextBtn.addEventListener('click', () => {
+        const nextIndex = (slideIndex + 1 + slides.length) % slides.length;
+        goToSlide(nextIndex);
     });
 
-    // active될 인디케이터 표시
-    indicators[index].classList.add('active');
+    indicators.forEach((indicator, index) => {
+        indicator.addEventListener('click', () => {
+            goToSlide(index);
+        });
+    });
 
-    slideIndex = index;
+    function autoSlide() {
+        const nextIndex = (slideIndex + 1) % slides.length;
+        goToSlide(nextIndex);
+    }
 
-    // 페이지 넘겼을 때 타이머 재설정
+    function resetAutoSlideTimer() {
+        clearInterval(autoSlideTimer); // 타이머 취소하고 정지 후 다시 
+        autoSlideTimer = setInterval(autoSlide, 5000);
+    }
+    goToSlide(0);
     resetAutoSlideTimer();
-}
-
-// 이전 버튼 클릭 시
-prevBtn.addEventListener('click', () => {
-    const prevIndex = (slideIndex - 1 + slides.length) % slides.length;
-    goToSlide(prevIndex);
 });
-
-// 다음 버튼 클릭 시
-nextBtn.addEventListener('click', () => {
-    const nextIndex = (slideIndex + 1 + slides.length) % slides.length;
-    goToSlide(nextIndex);
-});
-
-indicators.forEach((indicator, index) => {
-    indicator.addEventListener('click', () => {
-        goToSlide(index);
-    });
-});
-
-function autoSlide() {
-    const nextIndex = (slideIndex + 1) % slides.length;
-    goToSlide(nextIndex);
-}
-
-function resetAutoSlideTimer() {
-    clearInterval(autoSlideTimer); // 타이머 취소하고 정지 후 다시 
-    autoSlideTimer = setInterval(autoSlide, 5000);
-}
-
-resetAutoSlideTimer();
