97174c24beae4cd9fa49463df8e6d99fa6bc0bb5
YoonHyeJin <emfkdlvnem@gmail.com>
2023-06-27 22:09:03 +0900

    auth.js 수정

--- js/auth.js
+++ js/auth.js
@@ -3,16 +3,21 @@ import { auth } from './firebaseConfig.js';
 
 export function updateLoginButton() {
     const loginButton = document.getElementById('header-btn-login');
+    const signupButton = document.getElementById('header-btn-signup');
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
             // 로그인한 상태
             loginButton.textContent = '로그아웃';
+            signupButton.style.visibility = 'hidden';
+            signupButton.style.opacity = '0';
+            loginButton.style.visibility = 'visible';
+            loginButton.style.opacity = '1';
             loginButton.onclick = function() {
                 signOut(auth)
                     .then(() => {
                         console.log('로그아웃 성공');
-                        window.location.href = '../index.html'; // 로그인 페이지로 리다이렉트
+                        window.location.href = '../index.html'; 
                     })
                     .catch((error) => {
                         console.log('로그아웃 실패', error);
@@ -21,12 +26,28 @@ export function updateLoginButton() {
         } else {
             // 로그아웃 상태
             loginButton.textContent = '로그인';
+            signupButton.style.visibility = 'visible';
+            signupButton.style.opacity = '1';
+            loginButton.style.visibility = 'visible';
+            loginButton.style.opacity = '1';
             loginButton.onclick = function() {
-                window.location.href = './login.html'; // 로그인 페이지로 리다이렉트
+                window.location.href = './login.html'; 
             };
         }
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
