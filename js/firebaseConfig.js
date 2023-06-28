c85697e0f1e549d6f55cb6e7a95b71358a06bd4d
YoonHyeJin <emfkdlvnem@gmail.com>
2023-06-28 23:31:16 +0900

    api 교체 및 회원가입 기능 수정

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -2,13 +2,21 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCokCMiB2t_sqo7NMZfSY-duv0WzFoIq88",
+    // apiKey: "AIzaSyCokCMiB2t_sqo7NMZfSY-duv0WzFoIq88",
+    // authDomain: "koita-1c079.firebaseapp.com",
+    // projectId: "koita-1c079",
+    // storageBucket: "koita-1c079.appspot.com",
+    // messagingSenderId: "240009170899",
+    // appId: "1:240009170899:web:f9544fdb8a50ee45e56926",
+    // measurementId: "G-93MHVB0GZP"
+
+    apiKey: "AIzaSyBfXC9gasc0NI7bFx-BYN52ULFMjU9LLho",
     authDomain: "koita-1c079.firebaseapp.com",
     projectId: "koita-1c079",
     storageBucket: "koita-1c079.appspot.com",
     messagingSenderId: "240009170899",
-    appId: "1:240009170899:web:f9544fdb8a50ee45e56926",
-    measurementId: "G-93MHVB0GZP"
+    appId: "1:240009170899:web:a7184866ddf02275e56926",
+    measurementId: "G-NHQ6QB7LNY"
 };
 
 export const app = initializeApp(firebaseConfig);
--- js/signup.js
+++ js/signup.js
@@ -1,47 +1,83 @@
 
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, fetchSignInMethodsForEmail } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { auth, app } from './firebaseConfig.js';
 
+//Initialize firebase
+const analytics = getAnalytics(app);
 
+// 가입 성공 시 축하 팝업 뜨게 하기
+const signupPopup = document.querySelector('.popup-signup');
+signupPopup.style.display = 'none';
 
-    //Initialize firebase
-    const analytics = getAnalytics(app);
+// 일반 회원가입
+document.getElementById('btn-signup').addEventListener('click', (event) => {
+    event.preventDefault()
+    const emailSignup = document.getElementById('email-signup').value
+    const passwordSignup = document.getElementById('password-signup').value
+    
+    const passwordValidation = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/
+    
+    if (!passwordValidation.test(passwordSignup)) {
+        alert("비밀번호는 최소 6자 이상이어야 하며, 숫자와 알파벳을 각각 하나 이상 포함해야 합니다.");
+        return;
+    }
+    createUserWithEmailAndPassword(auth, emailSignup, passwordSignup)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+            signupPopup.style.display = 'block';
 
-    // 가입 성공 시 축하 팝업 뜨게 하기
-    const signupPopup = document.querySelector('.popup-signup');
-    signupPopup.style.display = 'none';
+            setTimeout(() => {
+                window.location.href = '../index.html';
+            }, 2000);
+            
+            console.log(userCredential)
+        })
+        .catch((error) => {
+            if (error.code === 'auth/email-already-in-use') {
+                alert('이미 사용중인 이메일입니다.');
+            } else {
+                console.log('error', error);
+                console.log('Error Code:', error.code);
+                console.log('Error Message:', error.message);
+            }
+        });
+});
 
-    // 일반 회원가입
-    document.getElementById('btn-signup').addEventListener('click', (event) => {
-        event.preventDefault()
-        const emailSignup = document.getElementById('email-signup').value
-        const passwordSignup = document.getElementById('password-signup').value
-        
-        createUserWithEmailAndPassword(auth, emailSignup, passwordSignup)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                // ...
-                signupPopup.style.display = 'block';
+// Google로 회원가입
+document.getElementById('btn-google-signup').addEventListener('click', (event) => {
+    event.preventDefault()
 
-                setTimeout(() => {
-                    window.location.href = '../index.html';
-                }, 2000);
-                
-                console.log(userCredential)
-            })
-            .catch((error) => {
-                if (error.code === 'auth/email-already-in-use') {
-                    // Show an error message if the email is already in use
-                    alert('이미 사용중인 이메일입니다.');
-                } else {
-                    console.log('error', error);
-                    console.log('Error Code:', error.code);
-                    console.log('Error Message:', error.message);
-                }
-            });
-    });
+    const provider = new GoogleAuthProvider();
 
-    console.log('welcome to koita');
-    console.log(app);
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+
+            fetchSignInMethodsForEmail(auth, user.email)
+                .then((signInMethods) => {
+                    if (signInMethods.length > 0) {
+                        alert('이미 가입된 아이디입니다.');
+                    } else {
+                        signupPopup.style.display = 'block';
+
+                        setTimeout(() => {
+                            window.location.href = '../index.html';
+                        }, 2000);
+                    }
+                });
+            
+            console.log(result)
+        }).catch((error) => {
+            console.log('error', error);
+            console.log('Error Code:', error.code);
+            console.log('Error Message:', error.message);
+        });
+});
+
+console.log('welcome to koita');
+console.log(app);
